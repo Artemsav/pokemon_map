dcf864753fd910c6f4695febfd8eb7f2ad275787
Artemsav <31589472+Artemsav@users.noreply.github.com>
2022-02-14 23:09:08 +0300

    change model, add addtional fields, adapt views

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,8 +3,10 @@ from django.db import models  # noqa F401
 # your models here
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, null = True, blank = True)
+    title_jp = models.CharField(max_length=200, null = True, blank = True)
     photo = models.ImageField(null = True, blank = True)
-
+    description =  models.TextField(null = True, blank = True)
     def __str__(self):
         return self.title
 
