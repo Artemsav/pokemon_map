df5f4f4b913eed9c9e397360d22b8e3c20b636f0
Artemsav <31589472+Artemsav@users.noreply.github.com>
2022-02-15 17:23:42 +0300

    evolution of pokemon added

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,6 +7,7 @@ class Pokemon(models.Model):
     title_jp = models.CharField(max_length=200, null = True, blank = True)
     photo = models.ImageField(null = True, blank = True)
     description =  models.TextField(null = True, blank = True)
+    parent = models.ForeignKey('self', on_delete=models.SET_NULL, null = True, blank = True, related_name='evolution')
     def __str__(self):
         return self.title
 
